4cfbce001242d7f34ef444ab4b400c69b6819ebb
plop <plop.dev0@gmail.com>
2025-06-25 19:25:29 +0100

    feat: add new dependencies and implement ThemeProvider and Toaster components for improved theming and notifications

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,19 +1,33 @@
+import { ThemeProvider } from '@/components/ThemeProvider';
+import { cookies } from 'next/headers';
 import React from 'react';
-import './styles.css';
+import { Toaster } from 'sonner';
+import NextTopLoader from 'nextjs-toploader';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
+import { Metadata } from 'next';
 
-export const metadata = {
-	description: 'A blank template using Payload in a Next.js app.',
-	title: 'Payload Blank Template',
+export const metadata: Metadata = {
+	title: 'Avium',
+	description: 'Avium offers cheap and simple 3d printing services',
 };
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
-	const { children } = props;
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+	const cookieStore = await cookies();
 
 	return (
-		<html lang='en'>
-			<body>
-				<main>{children}</main>
-			</body>
-		</html>
+		<>
+			<html lang='en' suppressHydrationWarning>
+				<head />
+				<body className='bg-background overflow-hidden overscroll-none font-sans antialiased'>
+					<NextTopLoader showSpinner={false}></NextTopLoader>
+					<NuqsAdapter>
+						<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange enableColorScheme>
+							<Toaster></Toaster>
+							{children}
+						</ThemeProvider>
+					</NuqsAdapter>
+				</body>
+			</html>
+		</>
 	);
 }
