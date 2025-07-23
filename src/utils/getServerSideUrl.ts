8dbc3fd0a058fbcb4aeb4e4dedeeac91b7dd7961
plop <plop.dev0@gmail.com>
2025-07-24 00:25:23 +0100

    fix url

--- src/utils/getServerSideUrl.ts
+++ src/utils/getServerSideUrl.ts
@@ -1,13 +1,16 @@
+/**
+ * INCLUDES HTTP(S)
+ *
+ * @returns The server URL as a string
+ */
 export const getServerSideURL = () => {
-	let url = process.env.NEXT_PUBLIC_SERVER_URL;
+	// either in dev, preview, or production
 
-	if (!url && process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-		return process.env.VERCEL_PROJECT_PRODUCTION_URL;
+	if (process.env.VERCEL_ENV === 'production') {
+		return 'https://' + process.env.VERCEL_PROJECT_PRODUCTION_URL;
+	} else if (process.env.VERCEL_ENV === 'preview') {
+		return 'https://' + process.env.VERCEL_BRANCH_URL;
+	} else {
+		return process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
 	}
-
-	if (!url) {
-		url = 'http://localhost:3000';
-	}
-
-	return url;
 };
