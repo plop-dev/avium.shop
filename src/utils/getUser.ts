91cd50e13e201e850f7ab6c9288828a04f040f18
plop <plop.dev0@gmail.com>
2025-07-23 17:35:48 +0100

    add shadcn's dashboard template

--- src/utils/getUser.ts
+++ src/utils/getUser.ts
@@ -4,24 +4,34 @@ import config from '@payload-config';
 import { headers as nextHeaders } from 'next/headers';
 import { User } from 'next-auth';
 
-export const getUser = async (): Promise<
-	| User
-	| (User & {
-			collection: 'users';
-	  })
-	| null
-> => {
+// export const getUser = async (): Promise<
+// 	| User
+// 	| (User & {
+// 			collection: 'users';
+// 	  })
+// 	| null
+// > => {
+// 	const session = await auth();
+
+// 	if (!session?.user) {
+// 		// user not logged in with authjs, but with local payload auth
+// 		const headers = await nextHeaders();
+
+// 		const payload = await getPayload({ config });
+// 		const user = await payload.auth({ headers });
+
+// 		return user.user;
+// 	}
+
+// 	return session?.user;
+// };
+
+export const getUser = async (): Promise<User | null> => {
 	const session = await auth();
 
 	if (!session?.user) {
-		// user not logged in with authjs, but with local payload auth
-		const headers = await nextHeaders();
-
-		const payload = await getPayload({ config });
-		const user = await payload.auth({ headers });
-
-		return user.user;
+		return null; // User is not authenticated
 	}
 
-	return session?.user;
+	return session.user; // Return the authenticated user
 };
