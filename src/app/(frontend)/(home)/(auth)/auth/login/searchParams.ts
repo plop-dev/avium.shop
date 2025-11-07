3b78499ab76be1ffa2a98174b7336ef676aabe43
maxime <plop.dev0@gmail.com>
2025-11-07 20:10:55 +0000

    clean up

--- src/app/(frontend)/(home)/(auth)/auth/login/searchParams.ts
+++
@@ -1,9 +0,0 @@
-import { createLoader, parseAsString } from 'nuqs/server';
-
-export const loginMessageSearchParams = {
-	error: parseAsString.withDefault(''),
-	success: parseAsString.withDefault(''),
-	message: parseAsString.withDefault(''),
-};
-
-export const loadSearchParams = createLoader(loginMessageSearchParams);
