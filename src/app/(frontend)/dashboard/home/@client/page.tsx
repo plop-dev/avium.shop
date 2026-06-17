f35d2cf36eda15bd0e7c34d3afe20ede5a1c7aef
maxime <plop.dev0@gmail.com>
2026-06-17 15:27:30 +0100

    implement client page with order retrieval and user context

--- src/app/(frontend)/dashboard/home/@client/page.tsx
+++ src/app/(frontend)/dashboard/home/@client/page.tsx
@@ -1,3 +1,29 @@
+import { getPayload } from 'payload';
+import config from '@payload-config';
+import { getUser } from '@/utils/getUser';
+
 export default async function ClientPage() {
-	return <h1>Client Page</h1>;
+	const payload = await getPayload({ config });
+	const user = await getUser();
+
+	const orders = await payload.find({
+		collection: 'orders',
+		where: {
+			customer: {
+				equals: user?.id,
+			},
+		},
+		limit: 0,
+		overrideAccess: true,
+	});
+
+	console.log(orders.docs);
+
+	return (
+		<div className='@container/main flex flex-1 flex-col gap-2'>
+			<div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6'>
+				<h1>content</h1>
+			</div>
+		</div>
+	);
 }
