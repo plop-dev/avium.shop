058669769a7fc8b3bde30355586291b563d5d34c
maxime <128096028+plop-dev@users.noreply.github.com>
2026-06-19 12:59:53 +0100

    migrate from middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,9 @@
 import NextAuth from 'next-auth';
 import { authConfig } from '@/auth.config';
-import { MiddlewareConfig } from 'next/server';
+import { ProxyConfig } from 'next/server';
 
 export const { auth: middleware } = NextAuth(authConfig);
 
-export const config: MiddlewareConfig = {
+export const config: ProxyConfig = {
 	matcher: ['/((?!api|_next/static|_next/image|favicon.ico|admin/login|order).*)'],
 };
