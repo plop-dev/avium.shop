3f6a826a7f05048da6d25c0d62ce97fe14c0ffe9
maxime <128096028+plop-dev@users.noreply.github.com>
2026-06-19 13:15:28 +0100

    update proxy and next config for v16

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,9 @@ const nextConfig: NextConfig = {
 			},
 		];
 	},
+	turbopack: {
+		root: process.cwd(),
+	},
 };
 
 export default withPayload(nextConfig);
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,13 @@
 import NextAuth from 'next-auth';
-import { authConfig } from '@/auth.config';
 import { ProxyConfig } from 'next/server';
+import { auth } from '@/auth';
 
-export const { auth: middleware } = NextAuth(authConfig);
+export const proxy = auth(req => {
+	if (!req.auth && req.nextUrl.pathname !== '/login') {
+		const newUrl = new URL('/login', req.nextUrl.origin);
+		return Response.redirect(newUrl);
+	}
+});
 
 export const config: ProxyConfig = {
 	matcher: ['/((?!api|_next/static|_next/image|favicon.ico|admin/login|order).*)'],
