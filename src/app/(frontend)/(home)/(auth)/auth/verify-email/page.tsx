e349f5198d8bee1af5a953b0007423aef52278ed
maxime <128096028+plop-dev@users.noreply.github.com>
2026-06-20 14:40:45 +0100

    fix build errors, and upgrade all payload sub packages

--- src/app/(frontend)/(home)/(auth)/auth/verify-email/page.tsx
+++ src/app/(frontend)/(home)/(auth)/auth/verify-email/page.tsx
@@ -1,3 +1,5 @@
+'use server';
+
 import { Suspense } from 'react';
 import { SearchParams } from 'nuqs/server';
 import { VerifyEmailContent } from './alertUser';
@@ -5,7 +7,6 @@ import { redirect } from 'next/navigation';
 import { getPayload } from 'payload';
 import config from '@payload-config';
 import { loadSearchParams } from './searchParams';
-import { NextResponse } from 'next/server';
 import { getServerSideURL } from '@/utils/getServerSideUrl';
 
 export default async function VerifyEmailPage({ searchParams }: { searchParams: Promise<SearchParams> }) {
@@ -25,21 +26,21 @@ export default async function VerifyEmailPage({ searchParams }: { searchParams: 
 			});
 		} catch (error) {
 			console.error('Error verifying email:', error);
-			return NextResponse.redirect(
+			redirect(
 				`${getServerSideURL}/auth/login?error=${encodeURIComponent('An error occurred verifying your email. Please try again.')}`,
 			);
 		}
 
 		if (res) {
-			return redirect(`/auth/login?success=${encodeURIComponent('Email verified successfully!')}`);
+			redirect(`/auth/login?success=${encodeURIComponent('Email verified successfully!')}`);
 		} else {
-			return redirect(`/auth/login?error=${encodeURIComponent('An error occured verifying your email. Please try again.')}`);
+			redirect(`/auth/login?error=${encodeURIComponent('An error occured verifying your email. Please try again.')}`);
 		}
 	}
 
 	// check if from or token is not defined in the URL (this means the user accessed this page directly, manually)
 	if (!from && !token) {
-		return redirect(`/auth/login?error=${encodeURIComponent('Invalid request. Please try again.')}`);
+		redirect(`/auth/login?error=${encodeURIComponent('Invalid request. Please try again.')}`);
 	}
 
 	return (
